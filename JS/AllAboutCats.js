6a5557d6597ae1e0749829a48b33644061b9076d
Jennifer Kreiner <jennifer.kreiner@edu.htl-klu.at>
2023-05-25 16:51:03 +0200

    started Catfacts

--- JS/AllAboutCats.js
+++ JS/AllAboutCats.js
@@ -1,4 +1,51 @@
 let formulare = document.getElementById('form');
+let factbutton = document.getElementById('buttonFact')
+let catFact = document.getElementById('catFact')
+
+
+
+
+fetch('https://catfact.ninja/fact?max_length=140')
+    .then(res => {
+        console.log(res.status);
+        return res.json();
+    })
+    .then(json => {
+        console.log("json", json);//asynchronous
+        addCatFact(json.fact);
+    })
+    .catch(err => console.error(err));
+
+function addCatFact(facts) {
+    let catFact = document.getElementById('catFact')
+    let content = "";
+
+    for (const theFact of facts) {
+        content += theFact.fact;
+    }
+    catFact.innerHTML = content;
+
+}
+
+factbutton.addEventListener('click',addCatFact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function formhandler(evt){
@@ -12,16 +59,16 @@ function formhandler(evt){
 
     if (inputValue == "YES"){
 
-    Array.from(forms).forEach(form => {
-        form.addEventListener('keypress', event => {
-            if (!form.checkValidity()){
-                evt.preventDefault();
-                evt.stopPropagation()
-            }
-            form.classList.remove('is-invalid');
-            form.classList.add('was-validated');
-        }, false)
-    })
+        Array.from(forms).forEach(form => {
+            form.addEventListener('keypress', event => {
+                if (!form.checkValidity()){
+                    evt.preventDefault();
+                    evt.stopPropagation()
+                }
+                form.classList.remove('is-invalid');
+                form.classList.add('was-validated');
+            }, false)
+        })
 
     }
 
